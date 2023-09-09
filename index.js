26036caedc92fde69974c360b5bd0507a661234f
Ashleigh McMahon <ashleighmc@me.com>
2023-09-09 11:06:48 -0600

    Lab Complete

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride) {
+
+  if (ride <= 400) {
+    return 'This one is on me!'
+  }
+  else if (ride >= 400 && ride <= 2000) {
+    return 'That will be twenty bucks.'
+  }
+  else if (ride >= 2000 && ride <=2500) {
+    return 'I will gladly take your thirty bucks.'
+  }
+  else if (ride >= 2500){
+    return 'No can do.'
+  }
   // Write your code here!
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  let result = city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+  return result;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  let response
+  switch  (tip){
+  case "generous":
+    response = 'Thank you so much.';
+    break;
+  case "not as generous":
+    response = 'Thank you.';
+    break;
+  default:
+    response=("Bye.");
+    break;
+  }
+  return (response)
 }
